84c4f7fb3386b920634f1740a6c0a23bd6b75bf3
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-08 12:19:57 +0100

    created accessapi package

---
+++ Source/ch/nolix/planningpokerapi/accessapi/IPlanningPokerAccess.java
@@ -0,0 +1,16 @@
+package ch.nolix.planningpokerapi.accessapi;
+
+import ch.nolix.coreapi.functionapi.mutationuniversalapi.IChangeSaver;
+import ch.nolix.planningpokerapi.datamodelapi.IRoom;
+import ch.nolix.planningpokerapi.datamodelapi.IUser;
+
+public interface IPlanningPokerAccess extends IChangeSaver {
+	
+	IRoom createAndEnterNewRoom(IUser user);
+	
+	IUser createUserWithName(String name);
+	
+	IRoom getRefRoomByIdentification(String identification);
+	
+	IUser getRefUserById(String id);
+}
