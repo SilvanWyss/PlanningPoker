a1621ad88816dee697f4ac55eee6720b4adf605e
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-09 20:37:42 +0100

    created PlanningPokerController

---
+++ Source/ch/nolix/planningpoker/business/PlanningPokerController.java
@@ -0,0 +1,69 @@
+package ch.nolix.planningpoker.business;
+
+import ch.nolix.core.errorcontrol.validator.GlobalValidator;
+import ch.nolix.planningpoker.datamodel.Room;
+import ch.nolix.planningpoker.datamodel.User;
+import ch.nolix.planningpokerapi.businessapi.IPlanningPokerController;
+import ch.nolix.planningpokerapi.datamodelapi.IRoom;
+import ch.nolix.planningpokerapi.datamodelapi.IUser;
+import ch.nolix.system.objectdatabase.database.DatabaseAdapter;
+
+public final class PlanningPokerController implements IPlanningPokerController {
+	
+	public static PlanningPokerController withDatabaseAdapter(final DatabaseAdapter databaseAdapter) {
+		return new PlanningPokerController(databaseAdapter);
+	}
+	
+	private final DatabaseAdapter databaseAdapter;
+	
+	private PlanningPokerController(final DatabaseAdapter databaseAdapter) {
+		
+		GlobalValidator.assertThat(databaseAdapter).thatIsNamed(DatabaseAdapter.class).isNotNull();
+		
+		this.databaseAdapter = databaseAdapter;
+	}
+	
+	@Override
+	public IRoom createAndEnterNewRoom(final IUser user) {
+		
+		final var room = Room.fromParentCreator((User)user);
+		databaseAdapter.insert(room);
+		
+		room.addVisitor(user);
+		
+		return room;
+	}
+	
+	@Override
+	public IUser createUserWithName(final String name) {
+		
+		final var user = User.withName(name);
+		databaseAdapter.insert(user);
+		
+		return user;
+	}
+	
+	@Override
+	public IRoom getRefRoomByIdentification(final String identification) {
+		return
+		databaseAdapter
+		.getRefTableByEntityType(Room.class)
+		.getRefEntities()
+		.getRefFirst(r -> r.getIdentification().equals(identification));		
+	}
+	
+	@Override
+	public IUser getRefUserById(final String id) {
+		return databaseAdapter.getRefTableByEntityType(User.class).getRefEntityById(id);
+	}
+	
+	@Override
+	public boolean hasChanges() {
+		return databaseAdapter.hasChanges();
+	}
+	
+	@Override
+	public void saveChanges() {
+		databaseAdapter.saveChangesAndReset();
+	}
+}
