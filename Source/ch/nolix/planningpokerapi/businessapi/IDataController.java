b709a0e97694e250e7f4e5be08488498c689ae9e
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-10 21:57:41 +0100

    created IDataController

---
+++ Source/ch/nolix/planningpokerapi/businessapi/IDataController.java
@@ -0,0 +1,16 @@
+package ch.nolix.planningpokerapi.businessapi;
+
+import ch.nolix.coreapi.functionapi.mutationuniversalapi.IChangeSaver;
+import ch.nolix.planningpokerapi.datamodelapi.IRoom;
+import ch.nolix.planningpokerapi.datamodelapi.IUser;
+
+public interface IDataController extends IChangeSaver {
+	
+	IRoom createAndEnterNewRoom(IUser user);
+	
+	IUser createUserWithName(String name);
+	
+	IRoom getRefRoomByIdentification(String identification);
+	
+	IUser getRefUserById(String id);
+}
--- Source/ch/nolix/planningpokerapi/businessapi/IPlanningPokerController.java
+++ Source/ch/nolix/planningpokerapi/businessapi/IPlanningPokerController.java
@@ -1,16 +1,6 @@
 package ch.nolix.planningpokerapi.businessapi;
 
-import ch.nolix.coreapi.functionapi.mutationuniversalapi.IChangeSaver;
-import ch.nolix.planningpokerapi.datamodelapi.IRoom;
-import ch.nolix.planningpokerapi.datamodelapi.IUser;
-
-public interface IPlanningPokerController extends IChangeSaver {
+public interface IPlanningPokerController {
 	
-	IRoom createAndEnterNewRoom(IUser user);
-	
-	IUser createUserWithName(String name);
-	
-	IRoom getRefRoomByIdentification(String identification);
-	
-	IUser getRefUserById(String id);
+	IDataController getRefDataController();
 }
