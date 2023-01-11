d6c8019c1946fe268199983c89d199bbcfd51a5e
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-11 21:27:28 +0100

    edited EventController

--- Source/ch/nolix/planningpoker/business/EventController.java
+++ Source/ch/nolix/planningpoker/business/EventController.java
@@ -7,16 +7,33 @@ import ch.nolix.planningpokerapi.businessapi.ISubscriber;
 
 public final class EventController implements IEventController {
 	
+	private boolean isUpdatingRoomSubscribers;
+	
+	private boolean updatingRoomSubscribersIsRequired;
+	
 	private final LinkedList<RoomSubscriber> roomSubscribers = new LinkedList<>();
 	
 	@Override
 	public void noteRoomChange(final String roomIdentification) {
-		
-		removeClosedRoomSubscribers();
-		
-		updateRoomSubscribersOfRoom(roomIdentification);
+		if (!isUpdatingRoomSubscribers()) {
+			
+			isUpdatingRoomSubscribers = true;
+			
+			updateRoomSubscribers(roomIdentification);
+			
+			while (updatingRoomSubscribersIsRequired()) {
+				
+				updatingRoomSubscribersIsRequired = false;
+				
+				updateRoomSubscribers(roomIdentification);
+			}
+			
+			isUpdatingRoomSubscribers = false;
+		} else {
+			updatingRoomSubscribersIsRequired = true;
+		}
 	}
-	
+
 	@Override
 	public void registerSubscriberForRoomChange(final String roomIdentification, final ISubscriber subscriber) {
 		registerRoomSubscriber(RoomSubscriber.forRoomAndSubscriber(roomIdentification, subscriber));
@@ -24,6 +41,10 @@ public final class EventController implements IEventController {
 	
 	private IContainer<RoomSubscriber> getRefRoomSubscribers() {
 		return roomSubscribers;
+	}
+	
+	private boolean isUpdatingRoomSubscribers() {
+		return isUpdatingRoomSubscribers;
 	}
 	
 	private void registerRoomSubscriber(final RoomSubscriber roomSubscriber) {
@@ -34,6 +55,16 @@ public final class EventController implements IEventController {
 		roomSubscribers.removeAll(RoomSubscriber::isClosed);
 	}
 	
+	private void updateRoomSubscribers(final String roomIdentification) {
+		removeClosedRoomSubscribers();
+		
+		updateRoomSubscribersOfRoom(roomIdentification);
+	}
+	
+	private boolean updatingRoomSubscribersIsRequired() {
+		return updatingRoomSubscribersIsRequired;
+	}
+
 	private void updateRoomSubscribersOfRoom(final String roomIdentification) {
 		for (final var rs : getRefRoomSubscribers()) {
 			if (rs.getRoomIdentification().equals(roomIdentification)) {
