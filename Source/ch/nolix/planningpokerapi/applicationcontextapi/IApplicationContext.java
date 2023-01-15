af7a096803d79457aed71800c99b8761af98a3ce
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-15 14:38:18 +0100

    created IApplicationContext

---
+++ Source/ch/nolix/planningpokerapi/applicationcontextapi/IApplicationContext.java
@@ -0,0 +1,5 @@
+package ch.nolix.planningpokerapi.applicationcontextapi;
+
+import ch.nolix.systemapi.applicationapi.mainapi.IApplicationControllerFactory;
+
+public interface IApplicationContext extends IApplicationControllerFactory<IApplicationController> {}
