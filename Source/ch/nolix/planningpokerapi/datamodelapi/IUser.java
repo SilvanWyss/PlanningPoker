a5eba2fa493754f2065ce7ae12b70b482059f660
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-27 12:07:10 +0100

    made IUser a Namable

--- Source/ch/nolix/planningpokerapi/datamodelapi/IUser.java
+++ Source/ch/nolix/planningpokerapi/datamodelapi/IUser.java
@@ -1,14 +1,11 @@
 package ch.nolix.planningpokerapi.datamodelapi;
 
 import ch.nolix.coreapi.attributeapi.mandatoryattributeuniversalapi.IdentifiedByString;
+import ch.nolix.coreapi.attributeapi.mutablemandatoryattributeuniversalapi.Namable;
 
-public interface IUser extends IdentifiedByString {
-	
-	String getName();
+public interface IUser extends IdentifiedByString, Namable {
 	
 	IRoomVisit getRefCurrentRoomVisit();
 	
 	boolean isInRoom();
-	
-	void setName(String name);
 }
