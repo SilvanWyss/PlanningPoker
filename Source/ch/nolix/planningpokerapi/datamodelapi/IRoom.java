0a34a9a5511ea51ce4a51e756eee57256a0e5800
Silvan Wyss <silvan.wyss@gmail.com>
2023-01-27 14:33:19 +0100

    made IRoom a IdentifiedByString

--- Source/ch/nolix/planningpokerapi/datamodelapi/IRoom.java
+++ Source/ch/nolix/planningpokerapi/datamodelapi/IRoom.java
@@ -1,8 +1,9 @@
 package ch.nolix.planningpokerapi.datamodelapi;
 
+import ch.nolix.coreapi.attributeapi.mandatoryattributeuniversalapi.IdentifiedByString;
 import ch.nolix.coreapi.containerapi.mainapi.IContainer;
 
-public interface IRoom {
+public interface IRoom extends IdentifiedByString {
 	
 	void addVisitor(IUser visitor);
 	
