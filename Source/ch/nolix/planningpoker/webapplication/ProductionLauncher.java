4b0607e656cf0e3dbb7c5312b8b8671d48f672e6
Nimeon <silvan.wyss@gmail.com>
2023-05-29 16:18:23 +0200

    created ProductionLauncher

---
+++ Source/ch/nolix/planningpoker/webapplication/ProductionLauncher.java
@@ -0,0 +1,15 @@
+package ch.nolix.planningpoker.webapplication;
+
+import ch.nolix.system.application.main.SecureServer;
+
+final class ProductionLauncher {
+	
+	public static void main(String[] args) {
+		
+		final var secureServer = SecureServer.forHttpsPortAndDomainAndSSLCertificateFromNolixConfiguration();
+		
+		secureServer.addDefaultApplication(PlanningPokerApplication.withTemporaryNodeDatabase());
+	}
+	
+	private ProductionLauncher() {}
+}
