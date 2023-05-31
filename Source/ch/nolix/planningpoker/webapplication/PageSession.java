d2842178919d2bb6b774ea3c5426196ebb3c0fb2
Nimeon <silvan.wyss@gmail.com>
2023-05-31 23:11:18 +0200

    refactored PageSession

--- Source/ch/nolix/planningpoker/webapplication/PageSession.java
+++ Source/ch/nolix/planningpoker/webapplication/PageSession.java
@@ -4,63 +4,36 @@ import ch.nolix.planningpokerapi.applicationcontextapi.IApplicationContext;
 import ch.nolix.planningpokerapi.applicationcontextapi.IDataController;
 import ch.nolix.system.application.webapplication.BackendWebClientSession;
 import ch.nolix.system.webgui.control.Label;
-import ch.nolix.system.webgui.linearcontainer.HorizontalStack;
 import ch.nolix.system.webgui.linearcontainer.VerticalStack;
+import ch.nolix.systemapi.webguiapi.containerapi.ContainerRole;
 import ch.nolix.systemapi.webguiapi.controlapi.LabelRole;
 import ch.nolix.systemapi.webguiapi.mainapi.IControl;
 
 public abstract class PageSession extends BackendWebClientSession<IApplicationContext> {
 	
+	private static final PageSessionHelper PAGE_SESSION_HELPER = new PageSessionHelper();
+	
 	protected abstract IControl<?, ?> createMainControl(IDataController dataController);
-	
-	protected final String getUserId() {
-		return getOriParentClient().getCookieValueByCookieNameOrNull("userId");
-	}
-	
-	protected final boolean hasUserId() {
-		return getOriParentClient().containsSessionVariableWithKey("userId");
-	}
 	
 	@Override
 	protected void initialize() {
 		try (final var dataController = getOriApplicationContext().createDataController()) {
-			getOriGUI().pushLayerWithRootControl(createRootControl(dataController));
+			getOriGUI()
+			.pushLayerWithRootControl(
+				new VerticalStack()
+				.setRole(ContainerRole.OVERALL_CONTAINER)
+				.addControl(
+					new VerticalStack()
+					.addControl(
+						new Label()
+						.setRole(LabelRole.TITLE)
+						.setText(getApplicationName()),
+						new Label()
+						.setText(PAGE_SESSION_HELPER.getUserLabelText(dataController, this))
+					),
+					createMainControl(dataController)
+				)
+			);
 		}
-	}
-	
-	private IControl<?, ?> createHeaderControl(final IDataController dataController) {
-		
-		final var headerControl =
-		new HorizontalStack()
-		.addControl(
-			new Label()
-			.setRole(LabelRole.TITLE)
-			.setText(getApplicationName())
-		);
-		
-		if (getOriParentClient().containsSessionVariableWithKey("userId")) {
-			
-			final var userId = getOriParentClient().getSessionVariableValueByKey("userId");
-			
-			headerControl.addControl(createUserControl(userId, dataController));
-		}
-		
-		return headerControl;
-	}
-	
-	private IControl<?, ?> createRootControl(final IDataController dataController) {
-		return
-		new VerticalStack()
-		.addControl(
-			createHeaderControl(dataController),
-			createMainControl(dataController)
-		);
-	}
-	
-	private IControl<?, ?> createUserControl(final String userId, final IDataController dataController) {
-		
-		final var user = dataController.getOriUserById(userId);
-		
-		return new Label().setText("you: " + user.getName());
 	}
 }
