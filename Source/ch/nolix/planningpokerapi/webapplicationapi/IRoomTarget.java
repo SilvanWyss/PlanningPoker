a7d54c4212c9abb60425c6a0b4f45c9e596b9513
Nimeon <silvan.wyss@gmail.com>
2023-06-17 23:45:16 +0200

    created IRoomTarget

---
+++ Source/ch/nolix/planningpokerapi/webapplicationapi/IRoomTarget.java
@@ -0,0 +1,8 @@
+package ch.nolix.planningpokerapi.webapplicationapi;
+
+import ch.nolix.coreapi.programcontrolapi.targetuniversalapi.IApplicationInstanceTarget;
+
+public interface IRoomTarget extends IApplicationInstanceTarget {
+	
+	String getRoomNumber();
+}
