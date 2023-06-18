a5aec111d9a3c037c11704c19b05dbf76521c07f
Nimeon <silvan.wyss@gmail.com>
2023-06-18 11:35:16 +0200

    created RoomHyperlinkCreator

---
+++ Source/ch/nolix/planningpoker/webapplication/RoomHyperlinkCreator.java
@@ -0,0 +1,29 @@
+package ch.nolix.planningpoker.webapplication;
+
+import ch.nolix.planningpokerapi.datamodelapi.IRoom;
+import ch.nolix.planningpokerapi.webapplicationapi.IRoomTarget;
+import ch.nolix.systemapi.applicationapi.mainapi.IApplication;
+
+public final class RoomHyperlinkCreator {
+	
+	public IRoomTarget createRoomTarget(final IRoom room, final IApplication<?> application) {
+		
+		final var applicationTarget = application.asTarget();
+		
+		return
+		RoomTarget.forIpOrAddressNameAndPortAndApplicationInstanceNameAndRoomNumberAndSecurityLevelForConnections(
+			applicationTarget.getIpOrAddressName(),
+			applicationTarget.getPort(),
+			applicationTarget.getApplicationInstanceName(),
+			room.getNumber(),
+			applicationTarget.getSecurityLevelForConnections()
+		);
+	}
+	
+	public String createHyperlinkToRoom(final IRoom room, final IApplication<?> application) {
+		
+		final var roomTarget = createRoomTarget(room, application);
+		
+		return roomTarget.toURL();
+	}
+}
