04e47838343d78c8353a40d3da6a4492aa353b20
Nimeon <silvan.wyss@gmail.com>
2023-08-22 22:47:12 +0200

    edited setName method of User

--- Source/ch/nolix/planningpoker/datamodel/schema/User.java
+++ Source/ch/nolix/planningpoker/datamodel/schema/User.java
@@ -47,9 +47,9 @@ public final class User extends Entity implements IUser {
 	@Override
 	public void setName(final String name) {
 		
-		GlobalValidator.assertThat(name).thatIsNamed(LowerCaseCatalogue.NAME).isNotEmpty();
+		GlobalValidator.assertThat(name).thatIsNamed(LowerCaseCatalogue.NAME).isNotBlank();
 		GlobalValidator.assertThat(name).thatIsNamed(LowerCaseCatalogue.NAME).isNotLongerThan(MAX_NAME_LENGTH);
-		GlobalValidator.assertThat(name).thatIsNamed(LowerCaseCatalogue.NAME).matches("[a-zA-Z0-9]*");
+		GlobalValidator.assertThat(name).thatIsNamed(LowerCaseCatalogue.NAME).matches("[a-zA-Z0-9[.]\s]*");
 				
 		this.name.setValue(name);
 	}
