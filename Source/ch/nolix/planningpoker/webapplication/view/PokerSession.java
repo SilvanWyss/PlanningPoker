1e88df0c25d770c9f6c7182775485d745395f577
Nimeon <silvan.wyss@gmail.com>
2023-11-10 22:51:25 +0100

    used RoomComponent in PokerSession

--- Source/ch/nolix/planningpoker/webapplication/view/PokerSession.java
+++ Source/ch/nolix/planningpoker/webapplication/view/PokerSession.java
@@ -1,20 +1,12 @@
 package ch.nolix.planningpoker.webapplication.view;
 
 import ch.nolix.core.errorcontrol.validator.GlobalValidator;
-import ch.nolix.coreapi.programcontrolapi.triggerapi.ITriggerableSubscriber;
-import ch.nolix.planningpoker.webapplication.cardsetcomponent.CardSetComponent;
-import ch.nolix.planningpoker.webapplication.estimateoverviewcomponent.EstimateOverviewComponent;
-import ch.nolix.planningpoker.webapplication.roomanalysiscomponent.RoomAnalysisComponent;
-import ch.nolix.planningpoker.webapplication.roomheadercomponent.RoomHeaderComponent;
-import ch.nolix.planningpoker.webapplication.roommanagercomponent.RoomManagerComponent;
+import ch.nolix.planningpoker.webapplication.roomcomponent.RoomComponent;
 import ch.nolix.planningpoker.webapplication.userlinecomponent.UserLineComponent;
-import ch.nolix.planningpokerapi.datamodelapi.schemaapi.IRoomVisit;
 import ch.nolix.planningpokerapi.logicapi.applicationcontextapi.IDataAdapter;
-import ch.nolix.system.webgui.linearcontainer.HorizontalStack;
-import ch.nolix.system.webgui.linearcontainer.VerticalStack;
 import ch.nolix.systemapi.webguiapi.mainapi.IControl;
 
-public final class PokerSession extends PageSession implements ITriggerableSubscriber {
+public final class PokerSession extends PageSession {
 
   private final String userId;
 
@@ -34,31 +26,8 @@ public final class PokerSession extends PageSession implements ITriggerableSubsc
   }
 
   @Override
-  public void trigger() {
-    try (final var dataAdapter = getStoredApplicationContext().createDataSupplier()) {
-
-      final var user = dataAdapter.getStoredUserById(userId);
-
-      if (user.isInARoom()) {
-        refreshIfDoesNotHaveOpenDialog();
-      } else {
-
-        final var selectRoomSession = SelectRoomSession.withUserId(userId);
-
-        setNext(selectRoomSession);
-      }
-    }
-  }
-
-  @Override
   protected IControl<?, ?> createMainControl(final IDataAdapter dataAdapter) {
-
-    getStoredApplicationContext().getStoredRoomChangeNotifier().registerRoomSubscriberIfNotRegistered(roomId, this);
-
-    final var user = dataAdapter.getStoredUserById(userId);
-    final var roomVisit = user.getStoredCurrentRoomVisit();
-
-    return createMainControl(roomVisit, dataAdapter);
+    return new RoomComponent(userId, dataAdapter, this, SelectRoomSession::withUserId).getStoredControl();
   }
 
   @Override
@@ -69,17 +38,5 @@ public final class PokerSession extends PageSession implements ITriggerableSubsc
   @Override
   protected void noteSelfChange() {
     getStoredApplicationContext().getStoredRoomChangeNotifier().noteRoomChange(roomId);
-  }
-
-  private IControl<?, ?> createMainControl(final IRoomVisit roomVisit, final IDataAdapter dataAdapter) {
-    return new VerticalStack()
-      .addControl(
-        new RoomHeaderComponent(userId, this, dataAdapter, SelectRoomSession::withUserId).getStoredControl(),
-        new RoomManagerComponent(userId, this, dataAdapter).getStoredControl(),
-        new CardSetComponent(roomVisit.getId(), roomId, this, dataAdapter).getStoredControl(),
-        new HorizontalStack()
-          .addControl(
-            new EstimateOverviewComponent(roomVisit.getId(), roomId, this, dataAdapter).getStoredControl(),
-            new RoomAnalysisComponent(roomId, this, dataAdapter).getStoredControl()));
   }
 }
