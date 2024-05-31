70a12b29af8361511ad4b3988347af97bc2f9fb5
Silvan Wyss <silvan.wyss@gmail.com>
2024-05-31 16:45:43 +0200

    Update Nolix

--- Source/ch/nolix/planningpokerapi/webapplicationapi/controllerapi/IRoomTarget.java
+++ Source/ch/nolix/planningpokerapi/webapplicationapi/controllerapi/IRoomTarget.java
@@ -1,6 +1,6 @@
 package ch.nolix.planningpokerapi.webapplicationapi.controllerapi;
 
-import ch.nolix.coreapi.programcontrolapi.targetapi.IApplicationInstanceTarget;
+import ch.nolix.coreapi.netapi.targetapi.IApplicationInstanceTarget;
 
 public interface IRoomTarget extends IApplicationInstanceTarget {
 
